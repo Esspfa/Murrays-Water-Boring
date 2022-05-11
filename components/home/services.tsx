01a9fb8c3c33efd37e0bfffc3aefea0c902b8041
Sarmed Rizvi <sarmedrizvi@gmail.com>
2022-05-11 23:43:21 +0500

    updates

--- components/home/services.tsx
+++ components/home/services.tsx
@@ -4,9 +4,9 @@ import Card from './card';
 
 const Cards = () => {
   return (
-    <div className="mt-10">
+    <div className="mt-10 mx-5">
       <h1 className="text-center font-extrabold text-darkBlue text-3xl ">Some Of Our Services Include..</h1>
-      <div className="grid lg:grid-cols-3 md:grid-cols-1  lg:mx-40 mt-5 sm:gap-5 `sm:mx-10">
+      <div className="grid md:grid-cols-3 xs:grid-cols-2 grid-cols-1 lg:mx-40 mt-5 gap-5">
         <Card
           title="Some of Our Services Include..."
           paragraphs="We’re water boring specialists and provide a range of services for domestic and farm use as well as irrigation purposes."
